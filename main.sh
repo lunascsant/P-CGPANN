6b59d4f4427b20c201b095e31e459e43a5674e64
Luciana Nascimento <luciananssantana@gmail.com>
2022-07-05 17:51:47 -0300

    update main.sh

--- main.sh
+++ main.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 for i in {0..29}; do
-	for VARIABLE in "cc" "cmb" "cordic" "frg1" "pm1" "sct" "t481" "tcon" "vda" "alu2" "alu4" "cm85a" "cm151a" "cm162a" "cu" "x2"; do
+	for VARIABLE in "cc" "cmb" "pm1" "sct" "t481" "tcon" "vda" "alu2" "alu4" "cm85a" "cm151a" "cm162a" "cu" "x2"; do
 		./${VARIABLE} ${VARIABLE} exe_$((i+1)) $i
 	done
 done
